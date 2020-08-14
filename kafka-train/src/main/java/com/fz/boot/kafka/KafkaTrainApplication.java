5a794ac3e4535d7bf904f1a86476ae04c37fab61
fangzheng <fangzheng@cfgdc.net>
2020-08-14 09:02:40 +0800

    Initial commit

---
+++ kafka-train/src/main/java/com/fz/boot/kafka/KafkaTrainApplication.java
@@ -0,0 +1,13 @@
+package com.fz.boot.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaTrainApplication.class, args);
+    }
+
+}
