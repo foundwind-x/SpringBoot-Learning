a92be5e5ce2e2b1711b59e5eb0cdf9d56828f361
fangzheng <fangzheng@cfgdc.net>
2020-09-29 17:46:50 +0800

    kafka depency modify

--- kafka/src/main/java/com/fz/boot/kafka/consumer/DemoConsumer.java
+++ kafka/src/main/java/com/fz/boot/kafka/consumer/DemoConsumer.java
@@ -7,8 +7,6 @@ import com.fz.boot.kafka.common.LogConstant;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
