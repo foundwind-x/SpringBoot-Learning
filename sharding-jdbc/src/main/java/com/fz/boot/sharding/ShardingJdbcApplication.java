5a20872a5b1b479804d9c760be80cbfd065d352d
fangzheng <fangzheng@cfgdc.net>
2020-10-21 17:11:47 +0800

    sharding hikari和druid连接池配置

--- sharding-jdbc/src/main/java/com/fz/boot/sharding/ShardingJdbcApplication.java
+++ sharding-jdbc/src/main/java/com/fz/boot/sharding/ShardingJdbcApplication.java
@@ -1,12 +1,16 @@
 package com.fz.boot.sharding;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.transaction.jta.JtaAutoConfiguration;
 
+/**
+ * 注意：排除DruidDataSourceAutoConfigure.class，因为DruidDataSourceAutoConfigure默认用了SpringBoot顶级的配置spring.datasource.url
+ */
 @MapperScan("com.fz.boot.sharding.**.mapper*")
-@SpringBootApplication(exclude = {JtaAutoConfiguration.class})
+@SpringBootApplication(exclude = {JtaAutoConfiguration.class,DruidDataSourceAutoConfigure.class})
 public class ShardingJdbcApplication {
 
     public static void main(String[] args) {
